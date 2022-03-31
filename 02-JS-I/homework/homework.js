5e73de2d031e4ae325d7ff80be57f9f02e2db11a
daniel cucuma <herreracucumadaniel@gmail.com>
2022-03-31 16:37:10 -0500

    buleanos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "hola";
 const nuevoNum = "3";
 
 // Crea una variable booleana:
-const nuevoBool = null;
+var meEncantaHenry = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
